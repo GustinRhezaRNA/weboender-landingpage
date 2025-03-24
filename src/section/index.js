867a3e5456054fec3a18da3bb8efab33b14f8c39
Gustin Rheza <rezarna4@gmail.com>
2025-03-24 22:36:27 +0700

    minor fix

--- src/section/index.js
+++ src/section/index.js
@@ -2,7 +2,7 @@
 export {default as CTA} from './CTA';
 export {default as Contact} from './Contact';
 export {default as Footer} from './Footer';
-export {default as Header} from './header';
+export {default as Header} from './Header';
 export {default as Navbar} from './Navbar';
 export {default as Schedule} from './Schedule';
 export {default as Activities} from './Activities';
